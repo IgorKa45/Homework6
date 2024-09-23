8ad101cd16be6617f1ad2187827ff691ad1cbf10
IgorKa45 <igorkanasnik@gmail.com>
2024-09-23 10:06:15 +0400

    Сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
+
         byte a;
-        for ( a = 1; a <= 10; a++ ) {
+        for (a = 1; a <= 10; a++) {
             System.out.println(a);
         }
 
         System.out.println("Задание 2");
 
-        for ( a = 10; a >= 1; a--) {
+        for (a = 10; a >= 1; a--) {
             System.out.println(a);
         }
 
         System.out.println("Задание 3");
 
-        for ( a = 0; a < 17; a++) {
+        for (a = 0; a < 17; a++) {
             if (a % 2 == 0) {
                 System.out.println(a);
             }
@@ -22,7 +23,7 @@ public class Main {
 
         System.out.println("Задание 4");
 
-        for (a = 10; a >= -10 ; a--) {
+        for (a = 10; a >= -10; a--) {
             System.out.println(a);
         }
 
@@ -37,17 +38,18 @@ public class Main {
 
         System.out.println("Задание 6");
 
-        byte number;
-        for (number = 0; number < 100; number = (byte)(number + 7) ) {
-            System.out.println(number);
+        for (a = 7; a < 100; a = (byte) (a + 7)) {
+            System.out.println(a);
         }
 
         System.out.println("Задание 7");
-        for (short c = 1; c <= 512; c =(short)(c * 2)) {
+
+        for (short c = 1; c <= 512; c = (short) (c * 2)) {
             System.out.println(c);
         }
 
         System.out.println("Задание 8");
+
         byte month;
         int savings;
         for (month = 0, savings = 0; month < 13; month++, savings = savings + 29000) {
@@ -55,15 +57,16 @@ public class Main {
         }
 
         System.out.println("Задание 9");
-        for (month = 0, savings = 0; month < 13; month++, savings = (int)((savings + 29000) + (savings * 0.01f))) {
+
+        for (month = 0, savings = 0; month < 13; month++, savings = (int) ((savings + 29000) + (savings * 0.01f))) {
             System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей.");
         }
 
         System.out.println("Задание 10");
-        byte b;
 
+        byte b;
         for (b = 1; b <= 10; b++) {
-            System.out.println("2 * " + b + " = " + 2*b);
+            System.out.println("2 * " + b + " = " + 2 * b);
         }
     }
 }
